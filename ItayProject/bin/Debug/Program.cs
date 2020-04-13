6214b7298635522f55d4e9478d7668cabbc38d35
itayno <itayno@gmail.com>
2020-04-13 10:23:55 +0300

    Search Project

---
+++ ItayProject/bin/Debug/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SearchLibrary;
+using System.IO;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ItayProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char choice;
+            string SearchWordFromUser;
+            string SearchDirectoryFromUser;
+            SearchClass newSearchFile = new SearchClass();
+            newSearchFile.DisplayResults += new DisplaySearchResultsDelegate(printSearchResult);
+            newSearchFile.DisplayError += new SearchFindErrorDelegate(PrintSearchErrors);
+            SearchClass.SearchResult result = SearchClass.SearchResult.SuccessfulSearch;
+
+            try
+            {
+                do
+                {
+                    Console.Write("1. Enter file name to search.\n" +
+                                    "2. Enter file name to search + parent directory to search in.\n" +
+                                    "3. Exit.");
+                    Console.WriteLine(); ;
+                    choice = Console.ReadKey().KeyChar;
+                    Console.Clear();
+
+                    switch (choice)
+                    {
+                        case '1':
+                            Console.WriteLine("Insert filename");
+                            SearchWordFromUser = Console.ReadLine();
+                            Console.Clear();
+                            result = newSearchFile.AddNewSearch(SearchWordFromUser);
+                            Console.WriteLine("Start searching... \n-------------------------------------");
+                            Console.WriteLine(result.ToString());
+                            break;
+                            //IMPORTENT:The results are being saved and the developer can see that in the SQL Server Object explorer
+                        case '2':
+                            Console.WriteLine("Insert directory to search in");
+                            SearchDirectoryFromUser = Console.ReadLine();
+                            Console.WriteLine("Insert file name");
+                            SearchWordFromUser = Console.ReadLine();
+                            Console.Clear();
+                            result = newSearchFile.AddNewSearch(SearchWordFromUser, SearchDirectoryFromUser);
+                            Console.WriteLine("Start searching... \n-------------------------------------");
+                            Console.WriteLine(result.ToString());
+                            break;
+                        default:
+                            Console.Clear();
+                            break;
+
+                    }
+
+                } while (choice != '3');
+            }
+            catch (Exception ex)
+            {
+               
+
+                Console.WriteLine("An error has occured: " + ex.Message);
+                
+
+            }
+        }
+        static void printSearchResult(FileInfo[] filesAndDirs)
+        {
+            if (filesAndDirs.Length > 0)
+                foreach (FileInfo foundFile in filesAndDirs)
+                {
+                    Console.WriteLine(foundFile.FullName);
+                }
+        }
+        static void PrintSearchErrors(Exception ex)
+        {
+            Console.WriteLine("Exception: " + ex.Message);
+        }
+    }
+}
---
+++ SearchLibrary/SearchClass.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace SearchLibrary
+{
+    public delegate void DisplaySearchResultsDelegate(FileInfo[] filesAndDirs);
+    public delegate void SearchFindErrorDelegate(Exception ErrorMessage);
+    public class SearchClass
+    {
+        //Search results status
+        public enum SearchResult
+        {
+            SuccessfulSearch,
+            DirNotFound,
+            SearchError
+        }
+        //Every UI can implement search result display as it wish
+        public event DisplaySearchResultsDelegate DisplayResults;
+        public event SearchFindErrorDelegate DisplayError;
+
+        //Creating the DB Connection
+        TheSearchInPCEntities1 dbConection = new TheSearchInPCEntities1();
+        //Declare the search function
+        public SearchResult AddNewSearch(string searchWordFromUser)
+        {
+            return AddNewSearch(searchWordFromUser, @"C:\");
+        }
+
+        public SearchResult AddNewSearch(string searchWordFromUser, string fil)
+        {
+            SearchResult result = SearchResult.SuccessfulSearch;
+            try
+            {
+                SearchMethod newSearch = new SearchMethod();
+              
+                newSearch.RootFile = fil;
+                newSearch.SearchText = searchWordFromUser;
+                dbConection.SearchMethods.Add(newSearch);
+                dbConection.SaveChanges();
+                result = SearchFileIn(newSearch.SearchText, newSearch.RootFile);
+            }
+            catch (Exception ex)
+            {
+                result = SearchResult.SearchError;
+                
+                DisplayError(ex);
+            }
+            return result;
+
+        }
+        
+        //Declare the directory of searching
+        public SearchResult SearchFileIn(string filename)
+        {
+            SearchResult result = SearchResult.SuccessfulSearch;
+            SearchFileIn(filename, @"C:\");
+            return result;
+        }
+
+        public SearchResult SearchFileIn(string filename, string directoryPath)
+        {
+            SearchResult result = SearchResult.SuccessfulSearch;
+            SearchAnswer newSearchAnswer = new SearchAnswer();
+            newSearchAnswer.SearchName = filename.ToLower();
+            FileInfo[] filesAndDirs = null;
+            try
+            {
+                if (Directory.Exists(directoryPath))
+                {
+                    var dirInfo = new DirectoryInfo(directoryPath);
+                   
+                    {
+                        DirectoryInfo hdDirectoryInWhichToSearch = new DirectoryInfo(directoryPath);
+                        //filesAndDirs = hdDirectoryInWhichToSearch.GetFiles("*" + SearchName.ToLower() + "*.*", SearchOption.AllDirectories);
+                        filesAndDirs = hdDirectoryInWhichToSearch.GetFiles("*" + filename.ToLower() + "*.*");
+                        DisplayResults(filesAndDirs);
+                        //get all subdirs then operate SearchFileIn on each subdir
+                        var dirs = Directory.GetDirectories(directoryPath);
+                        foreach (var d in dirs)
+                        {
+                            SearchFileIn(filename, d); // recursive call
+                        }
+                    }
+                }
+                else result = SearchResult.DirNotFound;
+
+                if (filesAndDirs != null)
+                    foreach (FileInfo foundFile in filesAndDirs)
+                    {
+                        string fullName = foundFile.FullName;
+                        newSearchAnswer.SearchResult = fullName;
+                        //dbConection.SearchAnswers.Add(newSearchAnswer);
+                    }
+                //dbConection.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                result = SearchResult.SearchError;
+                //Console.Write(ex.ToString());
+                DisplayError(ex);
+            }
+            return result;
+        }
+
+
+
+    }
+}
